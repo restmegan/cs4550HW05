c8293d7713d28fa5d8434f5d12f6170fddcb2067
Homework Five <hwfive@guest.guest>
2021-02-23 23:37:57 +0000

    deploy works!

--- bulls/deploy.sh
+++ bulls/deploy.sh
@@ -3,28 +3,12 @@
 # Taken from class notes
 # https://github.com/NatTuck/scratch-2021-01/blob/master/notes-4550/08-server-state/notes.md
 
-export MIX_ENV=prod
-export PORT=4801
-export SECRET_KEY_BASE=insecure
-
 mix deps.get --only prod
 mix compile
-
-CFGD=$(readlink -f ~/.config/bulls)
-
-if [ ! -d "$CFGD" ]; then
-    mkdir -p $CFGD
-fi
-
-if [ ! -e "$CFGD/base" ]; then
-    mix phx.gen.secret > "$CFGD/base"
-fi
-
-SECRET_KEY_BASE=$(cat "$CFGD/base")
-export SECRET_KEY_BASE
 
 npm install --prefix ./assets
 npm run deploy --prefix ./assets
 mix phx.digest
 
 mix release
+
